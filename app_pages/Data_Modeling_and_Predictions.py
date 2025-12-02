a72bc26f7a98989c56c87e27b4f166a2c02bc896
SonalDhoke <st20336540@outlook.cardiffmet.ac.uk>
2025-12-02 11:20:04 +0000

    Delete app_pages directory

--- app_pages/Data_Modeling_and_Predictions.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def show():
-    st.title("🤖 Data Modeling and Predictions")
-    st.write("Model training & forecasting go here.")
--- app_pages/References.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def show():
-    st.title("📚 References")
-    st.write("Add documentation, citations, or links here.")
